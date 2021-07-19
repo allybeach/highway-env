79ab7871f484c767c021a1ce7eea4e2488013224
Allyson Beach <ajbeach@umass.edu>
2021-07-19 10:32:22 -0700

    upload the latest changes to env

--- test_new_env.py
+++ test_new_env.py
@@ -1,7 +1,16 @@
 import gym
+from matplotlib import pyplot as plt
 import modified_highway_env
 
 env = gym.make('modified_highway_env:combo-v0')
-obs = env.reset()
-obs, reward, done, info = env.step(env.action_space.sample())
-print(info)
+env.reset()
+
+for _ in range(5):
+    action = env.action_type.actions_indexes["IDLE"]
+    obs, reward, done, info = env.step(action)
+    env.render()
+
+plt.imshow(env.render(mode="rgb_array"))
+plt.show(block=False)
+plt.pause(1)
+plt.close()
